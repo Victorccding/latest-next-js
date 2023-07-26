e0a5f63b4f692908c258282365c6d2c9948c7586
Victorccding <82922344+Victorccding@users.noreply.github.com>
2023-07-26 09:49:09 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: "/resources",
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/resources/:path*',
+        destination: '/:path*',
+      },
+    ]
+  }
+};
 
 module.exports = nextConfig
