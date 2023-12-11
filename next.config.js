533f25f7eaf19b96397fe0cc2942b5d9aba08e13
Victor Ding <weikding@amazon.com>
2023-12-11 14:53:34 -0800

    test

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: "/resources",
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/resources/:path*',
-        destination: '/:path*',
-      },
-    ]
-  }
 };
 
 module.exports = nextConfig
